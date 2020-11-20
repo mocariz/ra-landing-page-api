b12b5d18b6a5da940161ca05e1f58d5da7946c41
Monica Mota Ocariz <monica.ocariz@ingaia.com.br>
2020-11-20 16:23:18 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c771ffcd3d4555a696a00ddf5cd75279'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'db4b86f1-b76a-44d0-8873-7177468280b8'
+};
